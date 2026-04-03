77825163fcd91a527c28283278a87e966d6be2dd
laura <laura.vieira2007cl@gmail.com>
2026-04-03 10:43:01 -0300

    Avaliacao de Programacao Dinamica

---
+++ enunciadoA.py
@@ -0,0 +1,8 @@
+# Simular um centro de distribuicao com file de pedidos.
+# priorizando pedidos por urgencia e organizar saidas logisticas
+
+import pandas as pd;
+import matplotlib.pyplot as plt;
+import numpy as np;
+
+df = pd.read_csv ('Check_point_1_dados_logistica_RA_final_par.csv')
